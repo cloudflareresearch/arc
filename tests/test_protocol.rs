a7c3b35bb26133b6efc8e2a9640fe71b258f4c7a
Armando Faz <armfazh@cloudflare.com>
2026-04-17 20:27:11 -0700

    Add testing modules.

---
+++ tests/test_protocol.rs
@@ -0,0 +1,25 @@
+// Copyright (c) 2025 Cloudflare, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+mod arc_tests;
+
+#[cfg(feature = "suite_p256")]
+mod p256 {
+    crate::all_arc_tests!(arc::suites::P256);
+}
+
+#[cfg(feature = "suite_ristretto255")]
+mod ristretto255 {
+    crate::all_arc_tests!(arc::suites::Ristretto255);
+}
